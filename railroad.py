c920830c4a22a10fd85dcb9779559d49aa3be823
kopkins <kylehopkins1215@gmail.com>
2017-02-21 16:03:31 -0500

    add station selection

--- railroad.py
+++ railroad.py
@@ -7,12 +7,16 @@ from Train import Train
 
 class tool:
     def __init__(self):
-        self.api_path = 'http://www3.septa.org/hackathon/Arrivals/Suburban%20Station/{}/'
+        self.api_path = 'http://www3.septa.org/hackathon/Arrivals/{}/{}/'
+        self.station_id = 90005
         self.num_trains = 5
         self.trains = []
+        self.stations = []
+        self.stations_file = 'groomed_stations.csv'
         
         menu_options = []
         menu_options.append(('List Trains', lambda : self.fetch_trains()))
+        menu_options.append(('Select Station', lambda : self.select_station()))
         menu_options.append(('Set Number of Trains to Show',
                              lambda : self.count_limit()))
         menu_options.append(("Quit", lambda : exit()))
@@ -24,8 +28,25 @@ class tool:
         while True:
             self.main_menu.select_execute()
 
-    def pick_rail_line(self):
-        pass
+    def select_station(self):
+        if len(self.stations) == 0:
+            self.load_stations()
+        station_opts = []
+        for station in self.stations:
+            station_opts.append((station[1], lambda : self.set_station(station[0])))
+        station_menu = menu(station_opts)
+        station_menu.select_execute()
+
+
+    def load_stations(self):
+        with open(self.stations_file) as in_file:
+            for line in in_file:
+                id, name = line.strip().split(',')
+                self.stations.append((id, name))
+                
+
+    def set_station(self, station_id):
+        self.station_id = station_id
 
     def apply_filter(self, f, l):
         pass
@@ -34,7 +55,7 @@ class tool:
         self.num_trains = input('Trains to Display => ')
 
     def fetch_trains(self):
-        req = requests.get(self.api_path.format(self.num_trains))
+        req = requests.get(self.api_path.format(self.station_id, self.num_trains))
         self.build_trains(req.json())
         for train in self.trains:
             print(train)
